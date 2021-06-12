5f420b864e5c2e583cf29376ac16b58539821c9a
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2021-06-11 20:08:25 -0400

    set up cache installation

--- public/service-worker.js
+++ public/service-worker.js
@@ -9,3 +9,14 @@ const FILES_TO_CACHE = [
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png'
   ];
+const CACHE_NAME = "static-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+      caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    );
+    console.log('Installing to Cache');
+    self.skipWaiting();
+  });
+  
